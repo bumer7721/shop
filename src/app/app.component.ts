e42ffca089ad0f979b32dc5e639c6a5dccb15ce6
Volodymyr Hladkyi <Volodymyr_Hladkyi1@epam.comi>
2020-02-21 12:31:57 +0200

    used ViewChild directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild("appTitle", {static: false})
+  appTitleElem: ElementRef;
+
   title = 'Shop app';
+
+  ngAfterViewInit(): void {
+    console.log("After view init called");
+    this.appTitleElem.nativeElement.innerHTML = this.title;
+  }
 }
