3bacd7bbb1ffde342d6ce39eefb62c9e2e6ff954
Volodymyr Hladkyi <Volodymyr_Hladkyi1@epam.comi>
2020-02-23 18:50:14 +0200

    small improvement

--- src/app/shared/mouse-enter-backround.directive.ts
+++ src/app/shared/mouse-enter-backround.directive.ts
@@ -1,23 +1,28 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appMouseEnterBackround]'
 })
 export class MouseEnterBackroundDirective {
 
-  @HostBinding('style.backgroundColor')
-  backgroundColor: string;
+  private mouseEnterClasses = ' bg-info text-white';
+
+  @Input('class')
+  @HostBinding('class')
+  hostClasses: string;
 
   constructor( ) { }
 
   @HostListener('mouseenter')
   onMouseEnter() {
-    this.backgroundColor = 'lightblue';
+    console.log('Mouse enter!');
+    this.hostClasses += this.mouseEnterClasses;
   }
 
   @HostListener('mouseleave')
   onMouseLeave() {
-    this.backgroundColor = '';
+    console.log('Mouse leave!');
+    this.hostClasses = this.hostClasses.replace(this.mouseEnterClasses, '');
   }
 
 }
